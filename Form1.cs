1e70548420c28bd8c3c4eadfa62f2fac7f1713d7
xiaoxiao_Sean <rootbaucom45799@gmail.com>
2024-12-15 21:05:10 +0800

    再次完善功能，功能还没完善

--- Form1.cs
+++ Form1.cs
@@ -24,6 +24,8 @@ namespace 开机启动橘子桌面宠物_启动器
             if (!File.Exists(Application.StartupPath+"橘子桌面宠物.exe"))
             {
                 MessageBox.Show("未找到橘子桌面宠物，请将其放在与本程序相同的目录，并确认其名称，本程序所在目录为：" + Application.StartupPath);
+                this.Close();
+                return;
             }
             Process.Start(Application.StartupPath + "橘子桌面宠物.exe");
         }
